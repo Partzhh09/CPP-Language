f50cd96b77c27fa0534261ef592bd749979af5c9
Partzhh_9 <parthchotaliya788@gmail.com>
2025-07-02 10:05:54 +0530

    added some files

---
+++ print.cpp
@@ -0,0 +1,14 @@
+//print any name with cout
+
+// #include <iostream>
+// int main(){
+//     printf("Hello, World!\n");
+//     return 0;
+// }
+
+#include<iostream>
+using namespace std;
+int main(){
+    std::cout << "Hello World" <<endl;
+    return 0;
+}
